c01c69dd6d03002217d7bd348e6447cd6bb0c2c8
MikkoHuttunen <l4757@student.jamk.fi>
2020-08-06 19:27:30 +0300

    Learned some JavaScript

--- Lessons/Basics/app.js
+++ Lessons/Basics/app.js
@@ -152,3 +152,143 @@ luckyNumber === 7 ? console.log(luckyNumber, "LUCKY!") : console.log(luckyNumber
 let status = "online";
 
 let color2 = status === "offline" ? console.log("red") : console.log("green");
+
+//Example 12 (Arrays)
+let shoppingList = ["cereal", "cheese", "ice"];
+let lotto = [45, 12, 23, 25, 34];
+//Array can have different kind of values
+let myCollection = [12, 'dog', true, null, NaN];
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+console.log(colors.length);
+console.log(colors[2]);
+
+//Add new value to the end of an array
+shoppingList[shoppingList.length] = "milk";
+console.log(shoppingList);
+
+//Example 13 (Array methods)
+let topSongs = [
+    "First Time Ever I Saw Your Face", 
+    "God Only Knows", 
+    "a Day In The Life", 
+    "Life On Mars"
+];
+
+//Pushes new value to the end of an array
+topSongs.push("Fortunate Son");
+
+//Removes the last value of an array
+topSongs.pop();
+
+//Adds new value to the beginning of an array
+topSongs.unshift("Highway To Hell");
+
+//Removes the first value of an array
+topSongs.shift();
+
+let fruits = ["apple", "banana"];
+let vegetables = ["asparagus", "brussel sprouts"];
+
+//Merge arrays
+console.log(fruits.concat(vegetables));
+
+//Check if value exists in an array
+fruits.includes("orange");
+
+//Returns index number of a given value in an array
+vegetables.indexOf("brussel sprouts");
+
+//Reverses the values in an array
+vegetables.reverse();
+console.log(vegetables);
+
+//Concatenates an array in a single string
+console.log(fruits.join(", "));
+
+let animals = ["shark", "salmon", "whale", "bear", "lizard", "tortoise"];
+
+//Returns a copy of a certain portion of an array
+let swimmers = animals.slice(0, 3);
+console.log(swimmers);
+
+//Removes, replaces and adds new value to given index of an array
+animals.splice(1, 0, "octopus");
+
+//Returns sorted copy of a given array
+console.log(animals.sort());
+
+//Example 14 (Nested arrays)
+const animalPairs = [
+    ["doe", "buck"],
+    ["ewe", "ram"],
+    ["peahen", "peacock"]
+];
+
+//Access value in nested array
+console.log(animalPairs[2][0]);
+
+//Change value in nested array
+animalPairs[0][1] = "stag";
+
+//Example 15 (Objects)
+const fitbitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workaoutsThisWeek: "5 of 7",
+    avgGoodSleep: "2:13"
+};
+
+//Accessing value of an object
+console.log(fitbitData.totalMiles);
+
+const numbers = {
+    100: "one hundred",
+    16: "sixteen"
+};
+
+//Accessing value with a number as a key
+console.log(numbers[100]);
+
+//Empty object
+const userReviews = {};
+
+//Add value to object
+userReviews.mrSmith78 = 3.5;
+
+//Change value in object
+userReviews.mrSmith78++;
+
+//Nested object
+const student = {
+    firstName: "David",
+    lastName: "Jones",
+    strengths: ["Music", "Art"],
+    exams: {
+        midterm: 92,
+        final: 88
+    }
+};
+
+//Access values in nested objects
+console.log((student.exams.midterm + student.exams.final) / 2);
+
+//Array containing objects
+const shoppingCart = [
+    {
+        product: "Jenga Classic",
+        price: 6.88,
+        quantity: 1
+    },
+    {
+        product: "Echo Dot",
+        price: 29.99,
+        quantity: 3
+    },
+    {
+        product: "Fire Stick",
+        price: 39.99,
+        quantity: 2
+    }
+];
